80c10e2c39012e323f93831fd1eb231c3e0c806e
IMPRIVATAINC\radler <radler@imprivata.com>
2018-08-08 11:14:40 -0400

    Still trying to fix cloud 9 errors

--- Final Project/MovieSite/urls.py
+++ Final Project/MovieSite/urls.py
@@ -9,9 +9,7 @@ from django.contrib import admin
 import django.contrib.auth.views
 import app.forms
 import app.views
-import django
-
-
+from django.contrib.auth.views import logout
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
 # from django.contrib import admin
@@ -28,7 +26,7 @@ urlpatterns = [
     url(r'^$', app.views.home, name='home'), 
     url(r'^login/$',app.views.login, name='login'),
     url(r'^logout$',
-        django.contrib.auth.views.logout,
+        logout,
         {
             'next_page': '/',
         },
